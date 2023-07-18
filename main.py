1ae3f6467a2afff114aa91e98d4b1b626ce9b79c
KoshCocna <kostsiakimstar0109@gmail.com>
2023-07-19 09:58:21 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+screen.exitonclick()
