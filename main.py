077082204d2358d093d1cfe17247bafa77ed6c59
KoshCocna <kostsiakimstar0109@gmail.com>
2023-07-19 10:12:35 +1000

    third commit

--- main.py
+++ main.py
@@ -29,6 +29,14 @@ while game_is_on:
     ball.move()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    #the most tricky part here, Detect the collision with r_paddle
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        ball.bounce_x()
+
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
 screen.exitonclick()
